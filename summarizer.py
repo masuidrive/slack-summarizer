acbadeb6bb7800faf101959563ee051087f9835b
Yoonchul Koh <yoonchul.koh@gmail.com>
2023-03-04 18:31:56 +0900

    wip

--- summarizer.py
+++ summarizer.py
@@ -59,7 +59,7 @@ try:
         types="public_channel",
         exclude_archived=True,
     )
-    channel_names = list(map(lambda c: c["name"], channels_info))
+    channel_names = list(map(lambda c: c["name"], channels_info['channels']))
     print("channel_names: ", channel_names)
     channels = [channel for channel in channels_info['channels']
                 if not channel["is_archived"] and channel["is_channel"]]
