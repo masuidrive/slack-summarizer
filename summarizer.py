da87273a5428ad84566096c5e4b65da159a1898b
Yoonchul Koh <yoonchul.koh@gmail.com>
2023-03-04 23:33:00 +0900

    wip

--- summarizer.py
+++ summarizer.py
@@ -32,7 +32,7 @@ TOKEN = str(os.environ.get('SLACK_BOT_TOKEN')).strip()
 CHANNEL_ID = str(os.environ.get('SLACK_POST_CHANNEL_ID')).strip()
 
 # 取得する期間を計算する
-HOURS_BACK = 25
+HOURS_BACK = 25 * 2
 JST = pytz.timezone('Asia/Tokyo')
 now = datetime.now(JST)
 yesterday = now - timedelta(hours=HOURS_BACK)
