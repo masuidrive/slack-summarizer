8a46e6ad2dde617f1a06808029108c1192bc786c
Junya Okabe <86868255+Okabe-Junya@users.noreply.github.com>
2023-11-18 05:06:01 +0900

    update: gpt-model

--- summarizer.py
+++ summarizer.py
@@ -157,7 +157,7 @@ CHANNEL_ID = str(os.environ.get('SLACK_POST_CHANNEL_ID')).strip()
 LANGUAGE = str(os.environ.get('LANGUAGE') or "Japanese").strip()
 TIMEZONE_STR = str(os.environ.get('TIMEZONE') or 'Asia/Tokyo').strip()
 TEMPERATURE = float(os.environ.get('TEMPERATURE') or 0.3)
-CHAT_MODEL = str(os.environ.get('CHAT_MODEL') or "gpt-3.5-turbo").strip()
+CHAT_MODEL = str(os.environ.get('CHAT_MODEL') or "gpt-4-1106-preview").strip()
 DEBUG = str(os.environ.get('DEBUG') or "").strip() != ""
 MAX_BODY_TOKENS = 3000
 
